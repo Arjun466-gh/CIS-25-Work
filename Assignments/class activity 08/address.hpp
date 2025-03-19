0d4a878e0d8836145c174d82b68913aed53804e5
Arjun <YOUR_EMAIL>
2025-03-18 21:33:22 -0700

    added hpp file

---
+++ Assignments/class activity 08/address.hpp
@@ -0,0 +1,14 @@
+#ifndef ADDRESS_HPP
+#define ADDRESS_HPP
+
+#include <string>
+using namespace std;
+
+// Address structure definition
+struct Address {
+    string street;
+    string city;
+    int zipCode;
+};
+
+#endif
