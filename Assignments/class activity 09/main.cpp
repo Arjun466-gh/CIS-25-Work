afd0ea0915d08dc03c6d201072b1edf3610cdd3a
Arjun <YOUR_EMAIL>
2025-03-22 20:07:13 -0500

    Finished main file

--- Assignments/class activity 09/main.cpp
+++ Assignments/class activity 09/main.cpp
@@ -4,3 +4,8 @@ using namespace std;
 
 int main() {
     Movie myMovie;
+    myMovie.input();     // Take user input with validation
+    cout << "\nMovie Info:\n";
+    myMovie.print();     
+    return 0;
+}
